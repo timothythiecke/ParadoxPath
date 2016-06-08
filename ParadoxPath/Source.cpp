2c9cc371062a15185042dc117aa0bf8213d5df48
timothythiecke <timothy.thiecke@gmail.com>
2016-06-08 23:45:54 +0200

    Set up commit
    Added function

---
+++ ParadoxPath/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int FindPath(	const int nStartX, const int nStartY, ///< Start position (0-based)
+				const int nTargetX, const int nTargetY, ///< Target position (0-based)
+				const unsigned char* pMap, const int nMapWidth, const int nMapHeight, ///< map + dimensions
+				int* pOutBuffer, const int nOutBufferSize) ///< runtime map, and max items in map (consider dynamic reallocation), contains indices on original map
+{
+	// Return length of shortest path
+	return -1;
+}
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+}
